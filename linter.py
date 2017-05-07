b93933146747c1c24439cce5f250b663cc715643
AlexStep <alex.step.kellas@gmail.com>
2017-05-07 16:21:50 +0200

    base solium linter settings

---
+++ linter.py
@@ -0,0 +1,40 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by 
+# Copyright (c) 2017 
+#
+# License: MIT
+#
+
+"""This module exports the Solium plugin class."""
+
+from SublimeLinter.lint import NodeLinter, util
+
+
+class Solium(NodeLinter):
+    """Provides an interface to solium."""
+
+    syntax = 'solidity'
+    cmd = 'solium -R gcc --file @'
+    executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
+    regex = (
+        r'^.+?:(?P<line>\d+):(?P<col>\d+): '
+        r'(?:(?P<warning>warning)|(?P<error>error))'
+        r'(?P<message>.+)'
+    )
+    multiline = True
+    line_col_base = (1, 1)
+    tempfile_suffix = 'solidity'
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = r'\s*/[/*]'
+
